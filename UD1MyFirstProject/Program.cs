4c295e549357d1d313e3002a32dadd7933a2bddc
Gerard <gerard.all.92@gmail.com>
2021-05-13 11:04:24 +0200

    Initial commit

---
+++ UD1MyFirstProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UD1MyFirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
